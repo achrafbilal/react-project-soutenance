993ba1d65f8a619114358e64747febe820c691d7
Achraf Bilal <ach.20raf@outlook.fr>
2021-06-07 20:32:28 +0100

    30% project done
    Login template
    Register template
    Sidebar(Servers)
    Topbar

--- server/src/components/TopBar.jsx
+++ server/src/components/TopBar.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { Navbar, Container, Nav, Button } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import MenuIcon from '@material-ui/icons/Menu';
 
-function TopBar() {
+
+
+function TopBar({ auth, openSidebar }) {
+    const Links = (auth) => {
+        if (!auth.auth)
+            return (
+                <Container>
+
+                    <Nav className="me-auto">
+                        <Nav.Link as={NavLink} to="/login" exact >
+                            Login
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/register" exact >
+                            Register
+                </Nav.Link>
+                    </Nav>
+                </Container>
+            )
+        return (
+            <Container>
+                <Navbar.Brand onClick={() => openSidebar()} >
+                    <Nav.Link as={Button} variant="dark"   >
+                        <MenuIcon />
+                    </Nav.Link>
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link as={NavLink} to="/login" exact >
+                        Servers
+            </Nav.Link>
+                </Nav>
+
+            </Container >
+        )
+    }
     return (
-        <div>
-
-        </div>
+        <Navbar bg="dark" variant="dark" className="topbar">
+            <Links auth={auth()} />
+        </Navbar>
     )
 }
 
